1d23e041e31fd4f66e701c2b961da0d129bfe424
Muhammad Fajar Insan Kamil <insankamil3112@gmail.com>
2024-03-06 14:16:47 +0700

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./dist/css/main.css";
+import "./css/main.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
